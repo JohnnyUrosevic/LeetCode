f69bac0058372049a10ffbdce3a535d498e500b7
Johnny Urosevic <TheMagicalCake@gmail.com>
2018-12-27 19:58:59 -0800

    Reverse digits of signed integer

---
+++ 7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+                
+        int sum = 0;        
+        while (x != 0) {
+            if ((sum * 10) % 10 != 0) {
+                return 0;
+            } 
+            
+            sum *= 10;
+            
+            sum += x % 10;
+            
+            x /= 10;
+        }
+        return sum;
+    }
+};
