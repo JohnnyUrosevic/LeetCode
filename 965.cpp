cd6fe0012733ad78c5b2b5b4e834173725fc2776
Johnny Urosevic <TheMagicalCake@gmail.com>
2019-01-03 15:33:59 -0800

    Check if all values in tree are equal

---
+++ 965.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if (root == nullptr) return true;
+        
+        return (!root->left  || root->left->val  == root->val)
+            && (!root->right || root->right->val == root->val)
+            && isUnivalTree(root->left)
+            && isUnivalTree(root->right);
+    }
+};
