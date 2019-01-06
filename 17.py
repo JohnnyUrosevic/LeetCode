f8b2796bb98699ca96dabd8abbbfedcda204acbf
Johnny Urosevic <TheMagicalCake@gmail.com>
2019-01-05 22:07:05 -0800

    All possible phone number strings

---
+++ 17.py
@@ -0,0 +1,32 @@
+class Solution:
+    num_to_letters = {
+        '2': ["a", "b", "c"],
+        '3': ["d", "e", "f"],
+        '4': ["g", "h", "i"],
+        '5': ["j", "k", "l"],
+        '6': ["m", "n", "o"],
+        '7': ["p", "q", "r", "s"],
+        '8': ["t", "u", "v"],
+        '9': ["w", "x", "y", "z"]
+    }
+    
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        
+        oldList = []
+        currentList = self.num_to_letters[digits[0]]
+        
+        for digit in digits[1:] :
+            oldList = currentList
+            currentList = []
+            
+            #add each possible letter to the end of every previous string
+            for letter in self.num_to_letters[digit]:
+                currentList += [x + letter for x in oldList]
+
+        return currentList
