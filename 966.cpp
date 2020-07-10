83a42326c4dd3621cef5c76d408d958e6448ad22
Johnny Urosevic <TheMagicalCake@gmail.com>
2020-07-09 22:20:42 -0700

    Finished spellchecker
    ended up being a lot messier than thought

--- 966.cpp
+++ 966.cpp
@@ -1,65 +1,92 @@
 class Solution {
 public:
     vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
-        unordered_set<string> words;
-        unordered_map<string, string> lowerToCorrect;
-        unordered_map<string, int> lowerToIndex;
+        unordered_set<string> raw;
+        unordered_map<string, string> dictionary;
+        unordered_map<string, int> priority;
         
-        string vowels = "aeiou";
-        
-        for (int i = 0; i < wordlist.size(); i++) {
-            string word = wordlist[i];
+        for (size_t i = 0; i < wordlist.size(); i++) {
+            auto word = wordlist[i];
+            raw.insert(word);
             
-            words.insert(word);
+            string lower = "";
+            for (char c : word) {
+                lower += tolower(c); 
+            }
             
-            string lowerword = word;
-            transform(lowerword.begin(), lowerword.end(), lowerword.begin(), ::tolower);
-            if (lowerToCorrect.find(lowerword) != lowerToCorrect.end()) continue;
-            
-            lowerToCorrect[lowerword] = word;
-            lowerToIndex[lowerword] = i;
+            if (dictionary.find(lower) == dictionary.end()) {
+                dictionary[lower] = word;
+                priority[word] = i;
+            }
         }
         
         vector<string> result;
         
-        for (string query : queries) {
-            if (words.find(query) != words.end()) {
-                result.push_back(query);
+        
+        
+        for (auto word : queries) {
+            if (raw.find(word) != raw.end()) {
+                result.push_back(word);
                 continue;
             }
             
-            transform(query.begin(), query.end(), query.begin(), ::tolower);
-            
-            if (lowerToCorrect.find(query) != lowerToCorrect.end()) {
-                result.push_back(lowerToCorrect[query]);
+            string lower = "";
+            for (char c : word) {
+                lower += tolower(c); 
+            }
+
+            if (dictionary.find(lower) != dictionary.end()) {
+                result.push_back(dictionary[lower]);
                 continue;
             }
             
-            for (int i = 0; i < query.size(); i++) {
-                if (vowels.find(query[i]) != string::npos) {
-                    string correctWord = "";
-                    int minIndex = 5000;
-                    for (char vowel : vowels) {
-                        string tempWord = query;
-                        tempWord[i] = vowel;
-                        auto it = lowerToIndex.find(tempWord);
-                        if (it != lowerToIndex.end()) {
-                            if(it->second < minIndex) {
-                                minIndex = it->second;
-                                correctWord = lowerToCorrect[tempWord];
-                            }
-                        }
+            
+            auto possibilities = replace_vowels(lower);
+            
+            vector<string> matches;
+            for (auto possibility : possibilities) {
+                if (dictionary.find(possibility) != dictionary.end()) {
+                    matches.push_back(dictionary[possibility]);
+                }
+            } 
+            
+            int min = INT_MAX;
+            string min_word = "";
+            if (matches.size()) {
+                for (auto match : matches) {
+                    auto p = priority[match];
+                    if (p < min) {
+                        min = p;
+                        min_word = match;
                     }
-                    
-                    //check for multiple vowels ugh
                 }
             }
             
-
-            
-            result.push_back(""); //no match
+            result.push_back(min_word);
         }
         
         return result;
     }
+    
+    vector<string> replace_vowels(string word) {
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        
+        vector<string> possibilities;
+        for (size_t i = 0; i < word.size(); i++) {
+            if (vowels.find(word[i]) != vowels.end()) {
+                vector<string> subs = replace_vowels(word.substr(i+1));
+                string pre = word.substr(0, i);
+                for (auto v : vowels) {
+                    for (auto sub : subs) {
+                        possibilities.push_back(pre + v + sub);
+                    }
+                }
+            }
+        }
+        
+        if (possibilities.size() == 0) {
+            possibilities.push_back(word);
+        }
+        return possibilities;
+    } 
 };
